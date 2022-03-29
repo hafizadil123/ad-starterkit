46773a5f78cd7cb6aa5f09d32337c4a561c5a450
adil.sikandar <ahafiz167@gmail.com>
2022-03-29 18:39:51 +0500

    ad-starkit

---
+++ templates/node-mongodb/app/routes.js
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import UsersController from './controllers/user.controller';
+
+import errorHandler from './middleware/error-handler';
+
+const routes = new Router();
+
+// Users
+routes.post('/api/users/register', UsersController.register);
+routes.post('/api/users/register', UsersController.login);
+
+routes.use(errorHandler);
+
+export default routes;
